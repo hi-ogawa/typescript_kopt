4e27b777b76168dd1f4f9f79bdd84ce96c3d4af6
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-28 20:43:01 +0900

    refactor Kopt.two

--- src/kopt.ts
+++ src/kopt.ts
@@ -9,17 +9,13 @@ const two = (mtrx: Matrix) : Path => {
     const pairs = swappable_pairs(n);
     let path: Path = _.concat(_.range(n), [0]);
     while (true) {
-        let good_pair: Pair;
-        for (let k = 0; k < pairs.length; k++) {
-            const i = pairs[k][0];
-            const j = pairs[k][1];
-            if ( mtrx[path[i]][path[j]] + mtrx[path[i + 1]][path[j + 1]] <
-                 mtrx[path[i]][path[i + 1]] + mtrx[path[j]][path[j + 1]]
-               ) {
-                good_pair = [i, j];
-                break;
-            }
-        }
+        const good_pair = _.find(pairs, (pair) => {
+            const i = pair[0];
+            const j = pair[1];
+            const prev = mtrx[path[i]][path[i + 1]] + mtrx[path[j]][path[j + 1]];
+            const next = mtrx[path[i]][path[j]] + mtrx[path[i + 1]][path[j + 1]];
+            return next < prev;
+        });
         if (good_pair) {
             path = swap(path, good_pair);
         } else {
