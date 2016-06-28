67f84a666b9035f117286ab0085db5de270f34fd
hiogawa <hi.ogawa.zz@gmail.com>
2016-06-28 16:07:24 +0900

    implement 2-opt algorithm

--- src/kopt.spec.ts
+++ src/kopt.spec.ts
@@ -1,27 +1,28 @@
 import Kopt = require("./kopt");
 
 describe("Kopt", () => {
+    type Point = [number, number];
+    let vs: Point[];
+    vs = [ [0, 0]
+         , [1, 4]
+         , [2, 2]
+         , [3, 2]
+         , [3, 4]
+         , [6, 0]
+         , [7, 3]
+         , [8, 4]
+         , [9, 0]
+         ];
+    let f: (u: Point, v: Point) => number;
+    f = (u, v) => Math.sqrt(Math.pow(u[0] - v[0], 2) + Math.pow(u[1] - v[1], 2));
+
     describe("two", () => {
         it("", () => {
-            expect(Kopt.two).toBeDefined();
+            expect(Kopt.two(Kopt.mkMatrix(vs, f))).toEqual([ 0, 1, 4, 6, 7, 8, 5, 3, 2, 0 ]);
         });
     });
     describe("mkMatrix", () => {
         it("", () => {
-            type Point = [number, number];
-            let vs: Point[];
-                vs = [ [0, 0]
-                     , [1, 4]
-                     , [2, 2]
-                     , [3, 2]
-                     , [3, 4]
-                     , [6, 0]
-                     , [7, 3]
-                     , [8, 4]
-                     , [9, 0]
-                     ];
-            let f: (u: Point, v: Point) => number;
-                f = (u, v) => Math.sqrt(Math.pow(u[0] - v[0], 2) + Math.pow(u[1] - v[1], 2));
             expect(Kopt.mkMatrix(vs, f).length).toEqual(9);
         });
     });
--- src/kopt.ts
+++ src/kopt.ts
@@ -2,13 +2,55 @@ import _ = require("lodash");
 
 type Matrix = number[][];
 type Path = number[];
+type Pair = [number, number];
 
 const two = (mtrx: Matrix) : Path => {
-    return mtrx[0];
+    const n = mtrx.length;
+    const pairs = swappable_pairs(n);
+    let path: Path = _.concat(_.range(n), [0]);
+    while (true) {
+        let good_pair: Pair;
+        for (let k = 0; k < pairs.length; k++) {
+            const i = pairs[k][0];
+            const j = pairs[k][1];
+            if ( mtrx[path[i]][path[j]] + mtrx[path[i + 1]][path[j + 1]] <
+                 mtrx[path[i]][path[i + 1]] + mtrx[path[j]][path[j + 1]]
+               ) {
+                good_pair = [i, j];
+                break;
+            }
+        }
+        if (good_pair) {
+            path = swap(path, good_pair);
+        } else {
+            break;
+        }
+    }
+    return path;
+};
+
+const swap = (path: Path, pair: Pair) : Path => {
+    const i = pair[0];
+    const j = pair[1];
+    return _.concat(
+        path.slice(0, i + 1),
+        _.reverse(path.slice(i + 1, j + 1)),
+        path.slice(j + 1)
+    );
+};
+
+const swappable_pairs = (n: number) : Pair[] => {
+    const is = _.range(n);
+    return _.flatten(
+        is.map((i) =>
+            is.map((j) : Pair => [i, j]) // https://github.com/Microsoft/TypeScript/issues/2189
+               .filter((ij) => ij[1] - ij[0] >= 2)
+        )
+    );
 };
 
 const mkMatrix = <T>(vs: T[], f: ((u: T, v: T) => number)) : Matrix => {
-    return _.map(vs, (u) => _.map(vs, (v) => f(u, v)));
+    return vs.map((u) => vs.map((v) => f(u, v)));
 };
 
 export = {
